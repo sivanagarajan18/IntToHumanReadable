a5b8324c92968911b4a982c544dc40c56374aca6
Siva Nagarajan <sivan@salzertechnologies.com>
2021-07-07 23:13:21 +0530

    Initial Commit

---
+++ IntegerToHumanReadable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntegerToHumanReadable
+//
+//  Created by Salzer on 07/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
